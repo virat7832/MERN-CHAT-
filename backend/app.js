f7aa46cfc6dbc7dad0a383e815d0861e185714f5
Virat <viratsamdarshi@gmail.com>
2024-12-31 21:30:48 +0530

    Initial Changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 
-
 import express from 'express';
+import morgan from 'morgan';
 const app=express();
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
